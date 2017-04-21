3e0b53b745e13858f9f722b1dc57b5075c31e52e
zx <970389745@qq.com>
2017-04-22 04:34:48 +0800

    1

---
+++ src/main/java/com/zx/executor/LockAndConditionTest.java
@@ -0,0 +1,13 @@
+package com.zx.executor;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Lock类和Condition类的测试
+ */
+public class LockAndConditionTest {
+    //锁对象
+    final Lock lock = new ReentrantLock();
+
+}
